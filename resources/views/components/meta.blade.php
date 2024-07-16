57b9217e69493e66039b4157bcb185e3bb702ae0
mel <romelcub@gmail.com>
2024-07-16 13:52:31 +0800

    Updates : Tue, Jul 16, 2024  1:52:30 PM

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -2,4 +2,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="title" content="Manalo Resort Hotel">
+<meta name="description" content="Manalo Resort & Hotel, located in Mahabang Parang, Binangonan, Rizal. This serene haven, now the new home of Meta Animals • Manalo K9 • SDN • IAGD.">
+<meta name="keywords" content="manalo,manalok9,resort,hotel,manalo resort hotel,manalok9 resort hotel,meta animals,meta animals resort hotel">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="English">
+<meta name="author" content="Manalok9 IT WEB DEVELOPERS">
+
 <title>{{ $title }}</title>
